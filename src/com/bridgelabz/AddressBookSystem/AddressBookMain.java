dfadf9ebf174e3c4bd50738668e31d124105f9d8
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-26 19:24:42 +0530

    Added : Program for the Add new contact to address book using console

--- src/com/bridgelabz/AddressBookSystem/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookSystem/AddressBookMain.java
@@ -3,19 +3,9 @@ package com.bridgelabz.AddressBookSystem;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to address book system");
-		  ContactDetails newContact = new ContactDetails();
-	        newContact.setFirstName("Naveen\n");
-	        newContact.setLastName("Pattanashetti\n");
-	        newContact.setAddressCity("Bagalkot\n");
-	        newContact.setState("Karnataka\n");
-	        newContact.setEmail("pattanashettinaveen@gmail.com");
-	        newContact.setZip(587103);
-	        newContact.setPhoneNumber( 9844415351L);
-	        System.out.println("The Contact is :\n" +newContact);
-
-	    }
+		System.out.println("Welcome Address Book System");
+		NewAddressBook person = new NewAddressBook();
+		person.addContact();
 
 	}
-
-
+}
